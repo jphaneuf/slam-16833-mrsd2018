05b50fea16a7c0d674ab9fd483024a668ca28699
jphaneuf <phaneuf.joe>
2018-03-03 16:11:16 -0500

    get hl

---
+++ assignments/hw2/16833_hw2/EKF_slam/get_hl_single_symbolic.m
@@ -0,0 +1,11 @@
+% for some state p and control u at time t, predict state at next time step
+%Note: we're using a time step of 1 , so disregarding timestep. Note generalizable.
+
+function [ zi ] = get_hl_single_symbolic (lx,ly)
+  % robot x y theta , landmark x y
+  syms x y theta ; %lx ly ;
+  %Beta = wrapToPi ( atan2 ( ( ly - y ) , ( lx - x ) )  - theta )
+  Beta = atan2 ( ( ly - y ) , ( lx - x ) )  - theta;
+  r = sqrt ( ( ly - y )^2 + ( lx - x )^2 ) ;
+  zi = [ Beta r ];
+end
---
+++ assignments/hw2/16833_hw2/EKF_slam/get_hl_symbolic.m
@@ -0,0 +1,16 @@
+% for some state p and control u at time t, predict state at next time step
+%Note: we're using a time step of 1 , so disregarding timestep. Note generalizable.
+
+function [ h ] = get_hl_symbolic ()
+  % robot x y theta , landmark x y
+  %hardcoding some stuff, can't add landmarks 
+  syms x y theta lx1 ly1 lx2 ly2 lx3 ly3 lx4 ly4 lx5 ly5 lx6 ly6 ;
+  n_landmarks = 6;
+  landmarks   = [ lx1 ly1; lx2 ly2; lx3 ly3; lx4 ly4; lx5 ly5; lx6 ly6 ] ;
+  for i = 1 : n_landmarks
+    lx     = landmarks ( i , 1 );
+    ly     = landmarks ( i , 2 );
+    hi     = get_hl_single_symbolic ( lx , ly );
+    h(i,:) = hi;
+  end
+end
