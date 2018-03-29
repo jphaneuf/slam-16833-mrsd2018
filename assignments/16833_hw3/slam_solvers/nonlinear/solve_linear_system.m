6ee10d6c99afcd5a3b90d4d0b19f205aff623b15
jphaneuf <phaneuf.joe>
2018-03-29 18:26:46 -0400

    init solve_linear for nonlinear slam using chol2

--- assignments/16833_hw3/slam_solvers/nonlinear/solve_linear_system.m
+++ assignments/16833_hw3/slam_solvers/nonlinear/solve_linear_system.m
@@ -12,6 +12,28 @@
 %
 function x = solve_linear_system(A, b)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%% Your code goes here %%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%% Your code goes here %%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % SOLVE_CHOL2
+  % 16-831 Fall 2016 - *Stub* Provided
+  % Solves linear system using second Cholesky method
+  %
+  % Arguments: 
+  %     A     - A matrix from linear system that you generate yourself
+  %     b     - b vector from linear system that you generate yourself
+  %
+  % Returns:
+  %     x     - solution to the linear system, computed using the specified
+  %             version of the Cholesky decomposition
+  %     R     - R factor from the Cholesky decomposition
+  %
+  AtA = A' * A;  %one leg short of an at-at
+  %p = colamd ( AtA ); 
+  p = symrcm(AtA);
+  R = chol        ( AtA ( p , p ));
+  y = forward_sub ( R' , A ( : , p )'  * b )
+  x = back_sub    ( R  , y      )
+  x ( p ) = x
+
+end
