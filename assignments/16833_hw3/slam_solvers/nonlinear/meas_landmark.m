6781d1b2a2f0fd39cb314499d6c3bf1c7c354638
jphaneuf <phaneuf.joe>
2018-03-29 16:15:57 -0400

    measure landmark and jacobian

--- assignments/16833_hw3/slam_solvers/nonlinear/meas_landmark.m
+++ assignments/16833_hw3/slam_solvers/nonlinear/meas_landmark.m
@@ -9,10 +9,19 @@
 %     ly    - landmark's y position
 %
 % Returns:
-%     h     - odometry measurement prediction 
+%     h     - odometry measurement prediction = [ theta 
+%                                                   d  ]
 %
 function h = meas_landmark(rx, ry, lx, ly)
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%% Your code goes here %%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%% Your code goes here %%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  h = [ ];
+  theta = atan2 (   ly - ry     ,   lx - rx );
+  d     = sqrt  ( ( lx - rx )^2 + ( ly - ry )^2 );
+  h ( 1 , 1 ) = theta;
+  h ( 2 , 1 ) = d;
+end
+  
+  
