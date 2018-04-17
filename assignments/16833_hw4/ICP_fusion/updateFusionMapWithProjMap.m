360da069ac1882da59359d08d4db5d717acd9853
jphaneuf <phaneuf.joe>
2018-04-17 00:07:32 -0400

    update fusion

--- assignments/16833_hw4/ICP_fusion/updateFusionMapWithProjMap.m
+++ assignments/16833_hw4/ICP_fusion/updateFusionMapWithProjMap.m
@@ -4,7 +4,20 @@ function fusion_map = updateFusionMapWithProjMap(fusion_map, updated_map, h, w, 
 
     % Write your code here...
 
-    
+    map_points  = fusion_map.pointcloud.Location;
+    map_colors  = fusion_map.pointcloud.Color;
+    map_normals = fusion_map.normals;
+    map_ccounts = fusion_map.ccounts;
+    map_times   = fusion_map.times;
+    for i = proj_flag'
+      [ r  c  ] = ind2sub ( [ h , w ] , i );
+      map_points  ( i , : ) = updated_map.points  ( r , c , : );
+      map_colors  ( i , : ) = updated_map.colors  ( r , c , : );
+      map_normals ( i , : ) = updated_map.normals ( r , c , : );
+      map_ccounts ( i     ) = updated_map.ccounts ( i     );
+      map_times   ( i     ) = updated_map.times   ( i     );
+    end
+
     %==== Output the final point-based fusion map in a struct ====
     map_pointcloud = pointCloud(map_points, 'Color', map_colors);
     fusion_map = struct('pointcloud', map_pointcloud, 'normals', map_normals, 'ccounts', map_ccounts, 'times', map_times);
