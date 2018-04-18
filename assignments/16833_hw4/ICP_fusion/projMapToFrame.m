8ef02f10ffde81463e343973b96e6f38f432185b
jphaneuf <phaneuf.joe>
2018-04-18 00:30:50 -0400

    projMapToFrame:inv transform. return global points. ccounts pulls from fusion_map. normals doesnt round.

--- assignments/16833_hw4/ICP_fusion/projMapToFrame.m
+++ assignments/16833_hw4/ICP_fusion/projMapToFrame.m
@@ -15,8 +15,9 @@ function [proj_map, proj_flag] = projMapToFrame(fusion_map, h, w, tform, cam_par
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Prepare transforms %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    tform = tform.T'; %Heathens
-    R = tform ( 1:3 , 1:3 );
+    tform = inv ( tform.T' ); %Heathens
+    R =     inv ( tform ( 1:3 , 1:3 ) );
+    R =     tform ( 1:3 , 1:3 );
     %
     tform = tform ( 1:3 , : ); %4x4 => 3x4 extrinsic camera parameter
     K = [ fx 0 cx ; 0 fy cy ; 0 0 1 ];
@@ -29,9 +30,13 @@ function [proj_map, proj_flag] = projMapToFrame(fusion_map, h, w, tform, cam_par
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Apply transform %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%% Use proj_points_full to get image coordinates%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%% Use tform_points to track point cloud%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    cam_points_full  = tform * locs_t;
     proj_points_full = K * tform * locs_t;
     proj_points_full = proj_points_full';
+    cam_points_full  = cam_points_full';
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Get indices of points in front of camera %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -68,20 +73,22 @@ function [proj_map, proj_flag] = projMapToFrame(fusion_map, h, w, tform, cam_par
     %sparse inputs with duplicate indices will increment...
     %% i.e. x = [ 1 2 3 1 ] , y = [ 4 5 6 4 ] , v = ones () , will produce 2,5 = 1  , 3,6 = 1 , 1,4 = 2
     %%%Points are stored x/y , matrices row column
-    proj_ccounts  = full ( sparse ( proj_points_full ( valid_indices , 2 ) , proj_points_full ( valid_indices , 1 ) , ones ( 1 , length ( valid_indices ) ) , h , w ) );
-    proj_times    = full ( sparse ( proj_points_full ( valid_indices , 2 ) , proj_points_full ( valid_indices , 1 ) , fusion_map.times  ( valid_indices )   , h , w ) );
+    %proj_ccounts  = full ( sparse ( proj_points_full ( valid_indices , 2 ) , proj_points_full ( valid_indices , 1 ) , ones ( 1 , length ( valid_indices ) ) , h , w ) );
+    proj_ccounts  = full ( sparse ( proj_points_full ( valid_indices , 2 ) , proj_points_full ( valid_indices , 1 ) , fusion_map.ccounts ( valid_indices ) , h , w ) );
+    proj_times    = full ( sparse ( proj_points_full ( valid_indices , 2 ) , proj_points_full ( valid_indices , 1 ) , fusion_map.times   ( valid_indices ) , h , w ) );
 
-    proj_normals_full = round ( ( R * fusion_map.normals')' );
+    proj_normals_full = ( R * fusion_map.normals')';
     proj_points  = zeros ( h , w , 3 );
     proj_normals = zeros ( h , w , 3 );
     proj_colors  = zeros ( h , w , 3 );
     for i = valid_indices'
       x = proj_points_full ( i , 1 );
       y = proj_points_full ( i , 2 );
-      proj_points  ( y , x , : ) = locs                        ( i , 1:3 );
+      proj_points  ( y , x , : ) = cam_points_full             ( i , 1:3 );
       proj_normals ( y , x , : ) = proj_normals_full           ( i ,  :  ) ;
       proj_colors  ( y , x , : ) = fusion_map.pointcloud.Color ( i ,  :  ) ;
     end
+    fprintf ( 'valid projection indices: %d\n' , length ( valid_indices ) );
     proj_flag = valid_indices;
     proj_map = struct('points', proj_points, 'colors', proj_colors, 'normals', proj_normals, 'ccounts', proj_ccounts, 'times', proj_times);
         
