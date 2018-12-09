89319b1c5c20072be98b67b6cb8cbde351e253a2
jphaneuf <phaneuf.joe>
2018-12-09 18:55:33 -0500

    use pcdownsample in downsample

--- assignments/16833_hw4/ICP_fusion/downsampleData.m
+++ assignments/16833_hw4/ICP_fusion/downsampleData.m
@@ -1,10 +1,19 @@
 function [ds_pointcloud, ds_normals] = downsampleData(pointcloud, normals, ds_ratio)
     
+		%ds_ratio = 1/ds_ratio;
     %==== Downsample both pointcloud and normals ====
-    ds_points = pointcloud.Location(1:ds_ratio:end, 1:ds_ratio:end, :);
-    ds_colors = pointcloud.Color(1:ds_ratio:end, 1:ds_ratio:end, :);
-    ds_pointcloud = pointCloud(ds_points, 'Color', ds_colors);
-    ds_normals = normals(1:ds_ratio:end, 1:ds_ratio:end, :);
+    %ds_points = pointcloud.Location(1:ds_ratio:end, 1:ds_ratio:end, :);
+    %ds_colors = pointcloud.Color(1:ds_ratio:end, 1:ds_ratio:end, :);
+		%ds_points = imresize(pointcloud.Location,ds_ratio);
+		%ds_colors = imresize(pointcloud.Color,ds_ratio);
+
+    %ds_pointcloud = pointCloud(ds_points, 'Color', ds_colors);
+    %ds_normals = normals(1:ds_ratio:end, 1:ds_ratio:end, :);
+    %ds_normals = imresize(normals, ds_ratio);
+		ds_pointcloud = pcdownsample(pointcloud, 'gridaverage', ds_ratio);
+		temp_pc = pointCloud(normals);
+		temp_pc = pcdownsample(temp_pc, 'gridaverage', ds_ratio)
+		ds_normals = temp_pc.Location;
     
 end
     
